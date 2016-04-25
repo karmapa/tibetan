184584359733c882d3d0b314c930f2a976114d60
kmsheng <kmsh3ng@gmail.com>
2016-04-25 10:18:42 +0800

    remove fs related code

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
-var tibetDoc=require("./parse.js");
-var wylie=require("./wylie");
-var abridge=require("./abridge");
-module.exports={tibetDoc:tibetDoc,wylie:wylie,abridge:abridge}
+var wylie = require("./wylie");
+module.exports = wylie;
